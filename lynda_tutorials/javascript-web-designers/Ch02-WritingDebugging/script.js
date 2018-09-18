d2d7265c36365edb3db084cc8a57f95ec06354ef
michael-mm <mmuriithim@gmail.com>
2018-09-18 12:05:52 -0400

    Writing and Debugging

---
+++ lynda_tutorials/javascript-web-designers/Ch02-WritingDebugging/script.js
@@ -0,0 +1,17 @@
+// (function() {
+// "use strict";
+
+// console.log("Hello world.");
+// console.error("Oh no, something went wrong!");
+
+// })();
+
+(function() {
+"use strict";
+
+var variable1 = 2, variable2 = "hello";
+
+console.log(variable1)
+console.log("This is variable2: " + variable2);
+
+})();
