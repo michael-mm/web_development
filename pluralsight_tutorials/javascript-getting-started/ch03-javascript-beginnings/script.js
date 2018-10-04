08d9245105a85249471877a2b02acb2668d5a851
michael-mm <mmuriithim@gmail.com>
2018-10-04 15:23:43 -0400

    JavaScript Beginnings

---
+++ pluralsight_tutorials/javascript-getting-started/ch03-javascript-beginnings/script.js
@@ -0,0 +1,41 @@
+// 3. JavaScript Beginnings 
+
+// 3.2 Storing Info in Variables
+
+let productName = "Hammer",
+    productId = "H123";
+console.log(productName, productId); 
+
+
+// 3.4 Strings and Numeric Variables
+
+ let firstName = "Michael ",
+     lastName = "M";
+console.log(firstName + lastName)
+
+let price = 5.00, price2 = 3.00;
+console.log(price + price2);
+
+// 3.5 Simple Operators
+
+let first = 12,
+    second = 6;
+let answer = first % second;
+console.log(answer); // 0
+
+let totalCalc = 3 + 4 * 5;
+console.log(totalCalc); // 23
+
+let totalCalcParenthesis = (3 + 4) * 5;
+console.log(totalCalcParenthesis); // 35
+
+// 3.6 Commenting Code
+
+//
+// This is a comment
+//
+
+/* This is a comment */
+
+let name = "Slim Shady";  // an alias name
+console.log(name);
---
+++ pluralsight_tutorials/javascript-getting-started/ch03-javascript-beginnings/starting-blackjack-app.js
@@ -0,0 +1,15 @@
+// Starting Blackjack Application
+
+// Blackjack Simple Game App
+
+// Dealt two cards to start
+
+let card1 = "Ace of Spade",
+    card2 = "Ten of Hearts";
+
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
+
